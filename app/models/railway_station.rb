9dbad262cd3d76b8f81a71ad4787b531d7d404d7
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2016-05-13 23:50:55 +0300

    Users, tickets and links added.

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,2 +1,6 @@
 class RailwayStation < ActiveRecord::Base
+  has_many :railway_stations_routes
+  has_many :routes, through: :railway_stations_routes
+  has_many :start_tickets, class_name: 'Ticket', foreign_key: :start_station_id
+  has_many :end_tickets, class_name: 'Ticket', foreign_key: :end_station_id
 end
