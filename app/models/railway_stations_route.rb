e0ea51c66f13ee6ad700f860f2e7d6cbb23f824f
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2016-05-23 00:03:14 +0300

    Scope added.

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -4,4 +4,6 @@ class RailwayStationsRoute < ActiveRecord::Base
 
   validates :railway_station_id, uniqueness: { scope: :route_id }
   validates :number, uniqueness: { scope: :route_id }
+
+  scope :ordered, -> { order(:number) }
 end
