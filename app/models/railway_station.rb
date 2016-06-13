23e55c8a90669511e3c437a90830491193446b83
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2016-06-13 15:57:42 +0300

    Ru localization.

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,6 +4,8 @@ class RailwayStation < ActiveRecord::Base
   has_many :start_tickets, class_name: 'Ticket', foreign_key: :start_station_id
   has_many :end_tickets, class_name: 'Ticket', foreign_key: :end_station_id
 
+  validates :title, presence: true
+
   scope :ordered, -> { joins(:railway_stations_routes).order('railway_stations_routes.number').uniq }
 
   def number(route)
